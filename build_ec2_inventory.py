ef408895d0cc3899160838fa8b3a13c6da78e8db
Fyodor Markov <shhhowtime@mail.ru>
2021-11-21 00:26:13 +0700

    Modified for ec2

---
+++ build_ec2_inventory.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import json
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape()
+)
+
+template = env.get_template('inventory/inventory.yml.j2')
+
+f = open("inventory/inventory.json", "r")
+str = f.read()
+mas = json.loads(str)
+f.close()
+
+hosts = mas.get("tag_app_stage").get("hosts")
+masters = mas.get("tag_role_master").get("hosts")
+children = mas.get("tag_role_child").get("hosts")
+
+hostnames = {}
+for i in range(len(hosts)):
+    hostnames.update({hosts[i]: f"host{i+1}"})
+
+ffull_hosts=[]
+for host in hosts:
+    full_hosts.append(f"{hostnames.get(host)}: {{ ansible_host: {host} }}")
+
+master_hosts = []
+for host in masters:
+    master_hosts.append(f"{hostnames.get(host)}:")
+
+child_hosts = []
+for host in children:
+    child_hosts.append(f"{hostnames.get(host)}:")
+
+playbook = template.render(
+    host_list=full_hosts,
+    master_host_list=master_hosts,
+    child_host_list=child_hosts,
+)
+
+with open("inventory/stage/inventory.yaml", "w") as file:
+    file.write(playbook)
+
---
+++ run_stage.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#Installing dependencies
+pip3 install -r requirements.txt
+#Getting active ec2 hosts
+ansible-inventory -i inventory/aws_ec2.yaml --list > inventory/inventory.json
+#Building ansible inventory with hostnames and addresses
+python3 build_ec2_inventory.py
+#Running kubespray on this inventory
+ansible-playbook -i inventory/stage/inventory.yaml --become --become-user=root cluster.yml
